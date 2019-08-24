7bc70fd71965e14354f0ad4071c45de6e83c24f1
Malu202 <poehlmann.martin@gmail.com>
2019-08-24 23:47:48 +0200

    autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('css', function () {
     return gulp.src('./style.scss')
         .pipe(sass({ outputStyle: 'compressed', includePaths: 'node_modules' }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            cascade: false,
+            overrideBrowserslist: [
+                "> 0.2%",
+                "last 2 versions"
+            ]
+        }))
         .pipe(gulp.dest("./"));
 });
 
